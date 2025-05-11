a372d5b9859cc330dbf73d379bab20bf28eae211
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-05-11 11:21:31 +0700

    refactor(user): change something code #3

--- profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
@@ -7,24 +7,22 @@ import com.devteria.profile.service.UserProfileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/profiles")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class UserProfileController {
 
     UserProfileService userProfileService;
 
 
-    @PostMapping
-    UserProfileResponse createProfile(@RequestBody UserProfileRequest userProfileRequest){
-        return this.userProfileService.createProfile(userProfileRequest);
-    }
-
     @GetMapping("/{profileId}")
     UserProfileResponse getProfile (@PathVariable String profileId){
+        log.info("Get Profile have been called");
         return this.userProfileService.getProfile(profileId);
     }
 }
